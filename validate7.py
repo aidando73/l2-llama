eb3e12257c78b3fdf87555813c50205538d22cf5
Aidan Do <aidando73@gmail.com>
2025-01-08 19:49:02 +1100

    .

--- validate7.py
+++ validate7.py
@@ -17,7 +17,7 @@ eval_dir = sys.argv[1] if len(sys.argv) > 1 else None
 
 print("Applying patch...")
 os.system(f"cd {DJANGO_DIR} && git apply test.patch")
-print('\033[92mPatch applied\033[0m')
+print('Patch applied')
 
 if sample_row["version"] == "4.0":
     environment = "env_3_8"
