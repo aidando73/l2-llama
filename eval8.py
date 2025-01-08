5c8171e6c6725480aa62077efbceedabd59560b5
Aidan Do <aidando73@gmail.com>
2025-01-08 21:01:59 +1100

    .

--- eval8.py
+++ eval8.py
@@ -5,6 +5,7 @@ from llama_agent.agent23 import run_agent
 from llama_stack_client import LlamaStackClient
 from dotenv import load_dotenv
 import pandas as pd
+from subprocess import run
 
 load_dotenv()
 
@@ -18,7 +19,29 @@ if len(sys.argv) == 1:
 
 df = pd.read_parquet('test_data8.parquet')
 
+# Create sandbox directory if it doesn't exist
+os.makedirs(os.path.join(SCRIPT_DIR, "sandbox"), exist_ok=True)
 
-for index, row in df.iterrows():
-    instance_id = row['instance_id']
-    print(instance_id)
+# Create repo directories inside sandbox if they don't exist
+unique_repos = df['repo'].unique()
+for repo in unique_repos:
+    repo_name = repo.split('/')[-1]
+    repo_path = os.path.join(SCRIPT_DIR, "sandbox", repo_name)
+    if not os.path.exists(repo_path):
+        print(f"Cloning {repo} repository...")
+        run(f"git clone https://github.com/{repo}.git {repo_path}", shell=True, check=True)
+
+if not os.path.exists(os.path.join(SCRIPT_DIR, "sandbox", "ready.txt")):
+    # Django 4.0 uses python 3.8
+    # Django 4.1 and 4.2 use python 3.9
+    # Django 5.0 uses python 3.11
+    run(f"conda create -y -p {SCRIPT_DIR}/sandbox/django/env_3_8 python=3.8", shell=True, check=True)
+    run(f"conda create -y -p {SCRIPT_DIR}/sandbox/django/env_3_9 python=3.9", shell=True, check=True)
+    run(f"conda create -y -p {SCRIPT_DIR}/sandbox/django/env_3_11 python=3.11", shell=True, check=True)
+
+    # Sympy uses python 3.9
+    run(f"conda create -y -p {SCRIPT_DIR}/sandbox/sympy/env_3_9 python=3.9", shell=True, check=True)
+
+    # Marker file to indicate that the sandbox is ready
+    with open(os.path.join(SCRIPT_DIR, "sandbox", "ready.txt"), "w") as f:
+        f.write("Marker file")
