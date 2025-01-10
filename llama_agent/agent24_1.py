3aabf3a4b08f81dc3801ba5c8c6c84482b939e5f
Aidan Do <aidando73@gmail.com>
2025-01-10 14:25:23 +1100

    .

--- llama_agent/agent24_1.py
+++ llama_agent/agent24_1.py
@@ -3,6 +3,7 @@ from typing import Literal, Optional, Tuple, Union
 import re
 import json
 from llama_stack_client import LlamaStackClient, SamplingParams
+from llama_stack_client.types.shared_params.sampling_params import SamplingParams
 from llama_models.llama3.api.chat_format import ChatFormat
 from llama_models.llama3.api.tokenizer import Tokenizer
 from llama_models.llama3.api.datatypes import StopReason
