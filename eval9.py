e0ee00da86a367ccb099e32932bc4910c3b2b908
Aidan Do <aidando73@gmail.com>
2025-01-12 15:28:41 +1100

    .

--- eval9.py
+++ eval9.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 import os
 import sys
-from llama_agent.agent30 import run_agent
+from llama_agent.agent31 import run_agent
 from llama_stack_client import LlamaStackClient
 from dotenv import load_dotenv
 import pandas as pd
