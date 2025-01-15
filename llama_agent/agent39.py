c20f76bbe542e41ee2ece1d8aac82409ebb6e872
Aidan Do <aidando73@gmail.com>
2025-01-15 21:56:23 +1100

    .

--- llama_agent/agent39.py
+++ llama_agent/agent39.py
@@ -560,8 +560,8 @@ def run_agent(
                     difflib.unified_diff(
                         file_content.splitlines(keepends=True),
                         new_content.splitlines(keepends=True),
-                        fromfile="before",
-                        tofile="after",
+                        fromfile=file_chosen,
+                        tofile=file_chosen,
                     )
                 )
                 msg = "File successfully updated:\n" + "".join(diff)
