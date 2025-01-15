715577d5413c1a20053c2eed05dc471f24bc3689
Aidan Do <aidand@canva.com>
2025-01-15 22:04:40 +1100

    .

--- llama_agent/agent39.py
+++ llama_agent/agent39.py
@@ -642,7 +642,7 @@ def run_agent(
     print("Applying diff: " + green("".join(diff)))
 
     process = run(
-        ['git', 'apply'],
+        ['git', 'apply', '--verbose'],
         input="".join(diff).encode(),
         cwd=os.path.join(sandbox_dir, repo),
         capture_output=True
