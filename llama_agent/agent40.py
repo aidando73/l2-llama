9198a24cbd1d53e8240ce35d79caea7508b41668
Aidan Do <aidand@canva.com>
2025-01-16 15:18:25 +1100

    .

--- llama_agent/agent40.py
+++ llama_agent/agent40.py
@@ -555,7 +555,7 @@ def run_agent(
                     new_content = file_content.replace(search, replace)
                 
                 # Then try indent aware match
-                if search in file_content:
+                if new_content is None:
                     new_content = indent_aware_replace(file_content, search, replace)
                     if new_content is not None:
                         print("Indent aware match")
